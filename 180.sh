30987421fc8d51142c36c3c803d7e19b6953f959
peterYooTae <155613238+peterYooTae@users.noreply.github.com>
2024-08-27 03:13:38 +0900

    Add files via upload

---
+++ 180.sh
@@ -0,0 +1,8 @@
+sudo nvidia-smi -i 0 -pl 180
+sudo nvidia-smi -i 1 -pl 180
+sudo nvidia-smi -i 2 -pl 180
+sudo nvidia-smi -i 3 -pl 180
+sudo nvidia-smi -i 4 -pl 180
+sudo nvidia-smi -i 5 -pl 180
+sudo nvidia-smi -i 6 -pl 180
+sudo nvidia-smi -i 7 -pl 180
